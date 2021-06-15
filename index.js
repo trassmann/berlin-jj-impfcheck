2af4184586af491bbb5179595f581dcf3fa43040
Tormen Raßmann <tormen.rassmann@blogfoster.com>
2021-06-15 11:20:32 +0200

    auto doctors fetch

--- index.js
+++ index.js
@@ -1,56 +1,41 @@
 const axios = require("axios");
 const open = require("open");
+const cheerio = require("cheerio");
+const _ = require("lodash");
 
-const johnsonDoctorIds = [
-  "4955521",
-  "4351848",
-  "4914793",
-  "4386537",
-  "4529791",
-  "4407490",
-  "4781098",
-  "5099992",
-  "4463022",
-  "4351851",
-  "4879200",
-  "4860427",
-  "4385798",
-  "4948214",
-  "4407502",
-  "4907911",
-  "4407513",
-  "4596648",
-  "4407478",
-  "4954667",
-  "5110081",
-  "5005717",
-  "5026194",
-  "5325685",
-  "5723020",
-  "5250586",
-  "5421757",
-  "5421757",
-  "5271914",
-  "5716216",
-  "5628659",
-  "5325709",
-  "5588597",
-  "5246044",
-  "5916176",
-  "5246041",
-  "5325720",
-  "5478129",
-  "5288156",
-  "5455908",
-  "5431849",
-  "5410490",
-  "5304027",
-  "5304766",
-  "5865346",
-  "5484915",
-  "5325697",
-  "5880879",
-];
+const blacklistedNames = ["KANTPRAXIS"];
+
+const getDoctorIdsForPage = async (page) => {
+  let response = {};
+  try {
+    let url =
+      page === 1
+        ? "https://www.doctolib.de/impfung-covid-19-corona/berlin?ref_visit_motive_ids%5B%5D=7978"
+        : `https://www.doctolib.de/impfung-covid-19-corona/berlin?page=${page}&ref_visit_motive_ids%5B%5D=7978`;
+    response = await axios.get(url);
+
+    const $ = cheerio.load(response?.data);
+
+    const ids = [];
+    $(".dl-search-result").each((i, elem) => {
+      const [, , id] = elem.attribs.id.split("-");
+      ids.push(id);
+    });
+
+    return ids;
+  } catch (err) {
+    console.log(`ignored error for doctor search with page ${page}`);
+    return [];
+  }
+};
+
+const getAllDoctorIds = async () => {
+  const pages = [1, 2, 3, 4];
+
+  const doctorIdsRequests = pages.map(getDoctorIdsForPage);
+
+  return Promise.all(doctorIdsRequests).then((data) => _.uniq(_.flatten(data)));
+};
 
 const buildDoctorUrl = (doctorId) =>
   `https://www.doctolib.de/search_results/${doctorId}.json?limit=7&insurance_sector=public&ref_visit_motive_ids%5B%5D=7978&speciality_id=5593&search_result_format=json`;
@@ -75,8 +60,8 @@ const hasSlot = (doctorObj) =>
 
 const cooldowns = [];
 
-const run = async () => {
-  const doctorRequests = johnsonDoctorIds.map(fetchDoctorInfo);
+const run = async (doctorIds) => {
+  const doctorRequests = doctorIds.map(fetchDoctorInfo);
 
   await Promise.all(doctorRequests).then((data) => {
     const availableDoctors = data.filter(hasSlot);
@@ -84,7 +69,11 @@ const run = async () => {
     availableDoctors.forEach((doctor) => {
       const url = doctor?.search_result?.url;
 
-      if (url && !cooldowns.includes(url)) {
+      const isBlacklisted = blacklistedNames.includes(
+        doctor?.search_result?.name_with_title
+      );
+
+      if (url && !isBlacklisted && !cooldowns.includes(url)) {
         open(`https://www.doctolib.de${url}`);
         cooldowns.push(url);
 
@@ -93,13 +82,28 @@ const run = async () => {
           if (index > -1) {
             cooldowns.splice(index, 1);
           }
-        }, 60000);
+        }, 60000 * 2);
       }
     });
   });
 };
 
-setInterval(async () => {
-  console.log("Checking!");
-  await run();
-}, 1000);
+const init = async () => {
+  let allDoctorIds = await getAllDoctorIds();
+
+  setInterval(async () => {
+    console.log("Refetching doctor ids!");
+    allDoctorIds = await getAllDoctorIds();
+  }, 60000);
+
+  setInterval(async () => {
+    if (!_.isEmpty(allDoctorIds)) {
+      console.log("Checking!");
+      await run(allDoctorIds);
+    } else {
+      console.log("No doctor ids found.");
+    }
+  }, 2000);
+};
+
+init();
